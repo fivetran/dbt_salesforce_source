1daa34a9008a750f0da2bd9634a159bce450ac50
fivetran-kristin <kristin@fivetran.com>
2020-11-25 15:25:43 -0800

    remove unnecessary columns

--- models/stg_salesforce__opportunity.sql
+++ models/stg_salesforce__opportunity.sql
@@ -3,12 +3,16 @@ with source as (
     select *
     from {{ ref('stg_salesforce__opportunity_tmp') }}
 
-),
-
-renamed as (
+), macro as (
 
     select
-    
+        /*
+        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
+        that are expected/needed (staging_columns from dbt_github_source/models/tmp/) and compares it with columns 
+        in the source (source_columns from dbt_github_source/macros/).
+
+        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
+        */
         {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_salesforce__opportunity_tmp')),
@@ -16,7 +20,9 @@ renamed as (
             )
         }}
 
-        {% if var('opportunity_pass_through_columns') != [] %}
+      --The below script allows for pass through columns.
+
+        {% if var('opportunity_pass_through_columns') %}
         ,
         {{ var('opportunity_pass_through_columns') | join (", ")}}
 
@@ -24,9 +30,55 @@ renamed as (
 
     from source
 
-),
+), renamed as (
+    
+    select 
 
-calculated as (
+        _fivetran_synced,
+        account_id,
+        amount,
+        campaign_id,
+        close_date,
+        created_date,
+        description,
+        expected_revenue,
+        fiscal,
+        fiscal_quarter,
+        fiscal_year,
+        forecast_category,
+        forecast_category_name,
+        has_open_activity,
+        has_opportunity_line_item,
+        has_overdue_task,
+        id as opportunity_id,
+        is_closed,
+        is_deleted,
+        is_won,
+        last_activity_date,
+        last_referenced_date,
+        last_viewed_date,
+        lead_source,
+        name as opportunity_name,
+        next_step,
+        owner_id,
+        pricebook_2_id,
+        probability,
+        record_type_id,
+        stage_name,
+        synced_quote_id,
+        type
+
+      --The below script allows for pass through columns.
+
+        {% if var('opportunity_pass_through_columns') %}
+        ,
+        {{ var('opportunity_pass_through_columns') | join (", ")}}
+
+        {% endif %}
+        
+    from macro
+
+), calculated as (
         
     select 
         *,
@@ -38,9 +90,9 @@ calculated as (
         {{ dbt_utils.date_trunc('quarter', 'close_date') }} = {{ dbt_utils.date_trunc('quarter', dbt_utils.current_timestamp()) }} as is_closed_this_quarter
 
     from renamed
-    where not is_deleted
 
 )
 
 select * 
 from calculated
+where not is_deleted
--- models/stg_salesforce__user_role.sql
+++ models/stg_salesforce__user_role.sql
@@ -3,12 +3,16 @@ with source as (
     select *
     from {{ ref('stg_salesforce__user_role_tmp') }}
 
-),
-
-renamed as (
+), macro as (
 
     select
-    
+        /*
+        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
+        that are expected/needed (staging_columns from dbt_github_source/models/tmp/) and compares it with columns 
+        in the source (source_columns from dbt_github_source/macros/).
+
+        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
+        */
         {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_salesforce__user_role_tmp')),
@@ -16,16 +20,23 @@ renamed as (
             )
         }}
 
-        {% if var('user_role_pass_through_columns') != [] %}
-        ,
-        {{ var('user_role_pass_through_columns') | join (", ")}}
-
-        {% endif %}
-
     from source
+
+), renamed as (
+
+  select
+    _fivetran_deleted,
+    _fivetran_synced,
+    developer_name,
+    id as user_role_id,
+    name as user_role_name,
+    opportunity_access_for_account_owner,
+    parent_role_id,
+    rollup_description
+  from macro
 
 )
 
-select * 
+select *
 from renamed
 where not coalesce(_fivetran_deleted, false)
