20bb1dfdfc44fc8a5b4a507c41b87ea593006dfa
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2021-05-10 16:40:56 -0700

    Remove 'day' from Opportunity query
        org.apache.hive.service.cli.HiveSQLException: Error running query: org.apache.spark.sql.AnalysisException: cannot resolve '`day`' given input columns: [renamed._fivetran_synced, renamed.account_id, renamed.amount, renamed.campaign_id, renamed.close_date, renamed.created_date, renamed.description, renamed.expected_revenue, renamed.fiscal, renamed.fiscal_quarter, renamed.fiscal_year, renamed.forecast_category, renamed.forecast_category_name, renamed.has_open_activity, renamed.has_opportunity_line_item, renamed.has_overdue_task, renamed.is_closed, renamed.is_deleted, renamed.is_won, renamed.last_activity_date, renamed.last_referenced_date, renamed.last_viewed_date, renamed.lead_source, renamed.next_step, renamed.opportunity_id, renamed.opportunity_name, renamed.owner_id, renamed.probability, renamed.record_type_id, renamed.stage_name, renamed.synced_quote_id, renamed.type]; line 388 pos 8;

--- models/stg_salesforce__opportunity.sql
+++ models/stg_salesforce__opportunity.sql
@@ -83,8 +83,6 @@ with source as (
         *,
         created_date >= {{ dbt_utils.date_trunc('month', dbt_utils.current_timestamp()) }} as is_created_this_month,
         created_date >= {{ dbt_utils.date_trunc('quarter', dbt_utils.current_timestamp()) }} as is_created_this_quarter,
-        {{ dbt_utils.datediff(dbt_utils.current_timestamp(), 'created_date', 'day') }} as days_since_created,
-        {{ dbt_utils.datediff('close_date', 'created_date', 'day') }} as days_to_close,
         {{ dbt_utils.date_trunc('month', 'close_date') }} = {{ dbt_utils.date_trunc('month', dbt_utils.current_timestamp()) }} as is_closed_this_month,
         {{ dbt_utils.date_trunc('quarter', 'close_date') }} = {{ dbt_utils.date_trunc('quarter', dbt_utils.current_timestamp()) }} as is_closed_this_quarter
 
