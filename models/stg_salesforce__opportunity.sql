70e94d8b5e4ca1f2fb8a42db027d2a8c371c6bf1
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2021-05-11 10:20:40 -0700

    invoke DateDiff directly

--- models/stg_salesforce__opportunity.sql
+++ models/stg_salesforce__opportunity.sql
@@ -92,8 +92,8 @@ with source as (
         *,
         created_date >= {{ dbt_utils.date_trunc('month', dbt_utils.current_timestamp()) }} as is_created_this_month,
         created_date >= {{ dbt_utils.date_trunc('quarter', dbt_utils.current_timestamp()) }} as is_created_this_quarter,
-        {{ DATEDIFF('created_date',dbt_utils.current_timestamp()) }} as days_since_created,
-        {{ DATEDIFF('created_date','close_date') }} as days_to_close,
+        DATEDIFF('created_date',{{dbt_utils.current_timestamp()}}) as days_since_created,
+        DATEDIFF('created_date','close_date') as days_to_close,
         {{ dbt_utils.date_trunc('month', 'close_date') }} = {{ dbt_utils.date_trunc('month', dbt_utils.current_timestamp()) }} as is_closed_this_month,
         {{ dbt_utils.date_trunc('quarter', 'close_date') }} = {{ dbt_utils.date_trunc('quarter', dbt_utils.current_timestamp()) }} as is_closed_this_quarter
 
