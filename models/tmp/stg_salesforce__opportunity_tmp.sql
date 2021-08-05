efabd3f8f2810889059239ae3fdc4778048ed7e3
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-08-05 17:03:11 -0500

    salesforce history mode, docs creation, and postgres support

--- models/tmp/stg_salesforce__opportunity_tmp.sql
+++ models/tmp/stg_salesforce__opportunity_tmp.sql
@@ -1,2 +1,5 @@
 select *
 from {{ var('opportunity') }}
+{%- if var('salesforce__using_history_mode', false) -%}
+where _fivetran_active
+{%- endif -%}
--- models/tmp/stg_salesforce__user_role_tmp.sql
+++ models/tmp/stg_salesforce__user_role_tmp.sql
@@ -1,2 +1,5 @@
 select *
 from {{ var('user_role') }}
+{%- if var('salesforce__using_history_mode', false) -%}
+where _fivetran_active
+{%- endif -%}
