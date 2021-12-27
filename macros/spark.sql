6738670c91833e02b535c02cc5c21320bf9005a0
drernie <ernest@nauto.com>
2021-12-27 14:51:17 -0800

    revert opportunity

--- macros/spark.sql
+++
@@ -1,8 +0,0 @@
-{% macro spark__datediff(first_date, second_date, datepart) %}
-
-    DATEDIFF(
-        cast({{second_date}} as datetime),
-        cast({{first_date}} as datetime),
-    )
-
-{% endmacro %}
