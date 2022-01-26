0741b93a1990840cfc725cca7ddcad98fac41d39
fivetran-reneeli <renee.li@fivetran.com>
2022-01-26 09:16:40 -0800

    add enable config to tmp file

--- models/tmp/stg_salesforce__user_role_tmp.sql
+++ models/tmp/stg_salesforce__user_role_tmp.sql
@@ -1,3 +1,6 @@
+--To disable this model, set the using_user_role variable within your dbt_project.yml file to False.
+{{ config(enabled=var('salesforce__user_role_enabled', True)) }}
+
 select *
 from {{ var('user_role') }}
 {% if var('using_user_role_history_mode_active_records', false) %}
