3569ae666eaba3a7f9e98da9c12f4f090fa1f453
fivetran-reneeli <renee.li@fivetran.com>
2022-04-29 15:22:43 -0500

    formatting

--- models/stg_salesforce__user.sql
+++ models/stg_salesforce__user.sql
@@ -7,14 +7,6 @@ with base as (
 fields as (
 
     select
-        
-        /*
-        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
-        that are expected/needed (staging_columns from dbt_salesforce_source/models/tmp/) and compares it with columns 
-        in the source (source_columns from dbt_salesforce_source/macros/).
-
-        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
-        */
 
         {{
             fivetran_utils.fill_staging_columns(
@@ -24,11 +16,8 @@ fields as (
         }}
 
       --The below script allows for pass through columns.
-
         {% if var('user_pass_through_columns') %}
-        ,
-        {{ var('user_pass_through_columns') | join (", ")}}
-
+        , {{ var('user_pass_through_columns') | join (", ")}}
         {% endif %}
 
     from base
