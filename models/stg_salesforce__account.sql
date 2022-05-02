37ef1b779f03ddb55d4d1785f47c814ee22482cc
fivetran-reneeli <renee.li@fivetran.com>
2022-05-02 12:18:32 -0500

    fix identifier vars ref and formatting

--- models/stg_salesforce__account.sql
+++ models/stg_salesforce__account.sql
@@ -26,48 +26,48 @@ fields as (
 final as (
 
     select 
-      cast(_fivetran_synced as {{ dbt_utils.type_timestamp() }}) as _fivetran_synced,
-      account_number,
-      account_source,
-      cast(annual_revenue as {{ dbt_utils.type_numeric() }}) as annual_revenue,
-      billing_city,
-      billing_country,
-      billing_postal_code,
-      billing_state,
-      billing_state_code,
-      billing_street,
-      description,
-      id as account_id,
-      industry,
-      is_deleted,
-      cast(last_activity_date as {{ dbt_utils.type_timestamp() }}) as last_activity_date,
-      cast(last_referenced_date as {{ dbt_utils.type_timestamp() }}) as last_referenced_date,
-      cast(last_viewed_date as {{ dbt_utils.type_timestamp() }}) as last_viewed_date,
-      master_record_id,
-      name as account_name,
-      number_of_employees,
-      owner_id,
-      ownership,
-      parent_id,
-      rating,
-      record_type_id,
-      shipping_city,
-      shipping_country,
-      shipping_country_code,
-      shipping_postal_code,
-      shipping_state,
-      shipping_state_code,
-      shipping_street,
-      type,
-      website
+        cast(_fivetran_synced as {{ dbt_utils.type_timestamp() }}) as _fivetran_synced,
+        account_number,
+        account_source,
+        cast(annual_revenue as {{ dbt_utils.type_numeric() }}) as annual_revenue,
+        billing_city,
+        billing_country,
+        billing_postal_code,
+        billing_state,
+        billing_state_code,
+        billing_street,
+        description,
+        id as account_id,
+        industry,
+        is_deleted,
+        cast(last_activity_date as {{ dbt_utils.type_timestamp() }}) as last_activity_date,
+        cast(last_referenced_date as {{ dbt_utils.type_timestamp() }}) as last_referenced_date,
+        cast(last_viewed_date as {{ dbt_utils.type_timestamp() }}) as last_viewed_date,
+        master_record_id,
+        name as account_name,
+        number_of_employees,
+        owner_id,
+        ownership,
+        parent_id,
+        rating,
+        record_type_id,
+        shipping_city,
+        shipping_country,
+        shipping_country_code,
+        shipping_postal_code,
+        shipping_state,
+        shipping_state_code,
+        shipping_street,
+        type,
+        website
 
-      --The below script allows for pass through columns.
-      {% if var('account_pass_through_columns') %}
-      ,
-      {{ var('account_pass_through_columns') | join (", ")}}
+        --The below script allows for pass through columns.
+        {% if var('account_pass_through_columns') %}
+        ,
+        {{ var('account_pass_through_columns') | join (", ")}}
 
-      {% endif %}
-      
+        {% endif %}
+        
     from fields
 )
 
