e31a1cb60f778a80a06bd49462cfbd191531b5de
fivetran-reneeli <renee.li@fivetran.com>
2022-05-05 11:52:58 -0500

    revert change to account tmp

--- models/tmp/stg_salesforce__account_tmp.sql
+++ models/tmp/stg_salesforce__account_tmp.sql
@@ -1,10 +1,5 @@
 select *
 from {{ var('account') }}
-{% if var('using_account_history_mode_active_records', false) %}
-where 
-    {% if target.type == 'snowflake' %}
-        coalesce("_FIVETRAN_ACTIVE", true)
-    {% endif %}
-
-    coalesce(_fivetran_active, true)
+{% if var('using_opportunity_history_mode_active_records', false) %}
+where coalesce(_fivetran_active, true)
 {% endif %}
