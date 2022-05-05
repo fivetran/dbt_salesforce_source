09506a961f1e212bc01b9349a4f590f3676c6c92
fivetran-reneeli <renee.li@fivetran.com>
2022-05-05 10:52:54 -0500

    try snowflake conditional

--- models/tmp/stg_salesforce__account_tmp.sql
+++ models/tmp/stg_salesforce__account_tmp.sql
@@ -1,5 +1,10 @@
 select *
 from {{ var('account') }}
 {% if var('using_account_history_mode_active_records', false) %}
-where coalesce(_fivetran_active, true)
+where 
+    {% if target.type == 'snowflake' %}
+        coalesce("_FIVETRAN_ACTIVE", true)
+    {% endif %}
+    
+    coalesce((_fivetran_active, true)
 {% endif %}
