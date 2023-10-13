91c6f4dac618119aaa18fce1ba7eff6bb2df0bb2
Avinash Kunnath <avinash.kunnath@fivetran.com>
2023-10-13 07:15:49 -0700

    opportunity calculations

--- models/history/stg_salesforce__opportunity_history.sql
+++ models/history/stg_salesforce__opportunity_history.sql
@@ -62,8 +62,21 @@ final as (
         {{ fivetran_utils.fill_pass_through_columns('salesforce__opportunity_history_pass_through_columns') }}
 
     from fields
+),
+
+calculated as (
+        
+    select 
+        *,
+        created_date >= {{ dbt.date_trunc('month', dbt.current_timestamp_backcompat()) }} as is_created_this_month,
+        created_date >= {{ dbt.date_trunc('quarter', dbt.current_timestamp_backcompat()) }} as is_created_this_quarter,
+        {{ dbt.datediff(dbt.current_timestamp_backcompat(), 'created_date', 'day') }} as days_since_created,
+        {{ dbt.datediff('close_date', 'created_date', 'day') }} as days_to_close,
+        {{ dbt.date_trunc('month', 'close_date') }} = {{ dbt.date_trunc('month', dbt.current_timestamp_backcompat()) }} as is_closed_this_month,
+        {{ dbt.date_trunc('quarter', 'close_date') }} = {{ dbt.date_trunc('quarter', dbt.current_timestamp_backcompat()) }} as is_closed_this_quarter
+    from final
 )
 
-select *
-from final
+select * 
+from calculated
 where not coalesce(is_deleted, false)
