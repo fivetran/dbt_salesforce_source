fb850fc589f8ace983426f089adf2f8ff86ac788
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-06-28 21:34:56 -0500

    add casting

--- macros/coalesce_w_renamed_col.sql
+++ macros/coalesce_w_renamed_col.sql
@@ -1,5 +1,5 @@
 {% macro coalesce_w_renamed_col(original_column_name, datatype=none) %}
-
+{# This macro accomodates Fivetran connectors that keep the original salesforce field naming conventions without underscores #}
 {%- set renamed_col = original_column_name.replace('_', '') -%}
 
 {% if datatype is none -%}
