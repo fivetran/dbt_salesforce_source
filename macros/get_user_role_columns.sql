20658dcf70947869460f8ff5cd0e0b0df7ecaae8
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-06-28 19:50:40 -0500

    bug/renamed-columns

--- macros/get_user_role_columns.sql
+++ macros/get_user_role_columns.sql
@@ -5,11 +5,15 @@
     {"name": "_fivetran_active", "datatype": "boolean"},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "developer_name", "datatype": dbt.type_string()},
+    {"name": "developername", "datatype": dbt.type_string()},
     {"name": "id", "datatype": dbt.type_string()},
     {"name": "name", "datatype": dbt.type_string()},
     {"name": "opportunity_access_for_account_owner", "datatype": dbt.type_string()},
+    {"name": "opportunityaccessforaccountowner", "datatype": dbt.type_string()},
     {"name": "parent_role_id", "datatype": dbt.type_string()},
-    {"name": "rollup_description", "datatype": dbt.type_string()}
+    {"name": "parentroleid", "datatype": dbt.type_string()},
+    {"name": "rollup_description", "datatype": dbt.type_string()},
+    {"name": "rollupdescription", "datatype": dbt.type_string()}
 ] %}
 
 {{ fivetran_utils.add_pass_through_columns(columns, var('salesforce__user_role_pass_through_columns')) }}
