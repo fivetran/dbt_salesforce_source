f47bd73cd4737e6a2618eaa53eb8089edb86ff72
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-07-01 18:07:49 -0500

    macro updates

--- macros/coalesce_w_renamed_col.sql
+++ macros/coalesce_w_renamed_col.sql
@@ -1,10 +1,8 @@
-{% macro coalesce_w_renamed_col(original_column_name, datatype=dbt.type_string()) %}
+{% macro coalesce_w_renamed_col(original_column_name, datatype=dbt.type_string(), alias=original_column_name, renamed_column_name=original_column_name.replace('_', '')) %}
 {# This macro accomodates Fivetran connectors that keep the original salesforce field naming conventions without underscores #}
 
-{%- set renamed_col = original_column_name.replace('_', '') -%}
-
-coalesce(cast({{ renamed_col }} as {{ datatype }}),
+coalesce(cast({{ renamed_column_name }} as {{ datatype }}),
     cast({{ original_column_name }} as {{ datatype }}))
-    as {{ original_column_name }}
+    as {{ alias }}
 
 {%- endmacro %}
