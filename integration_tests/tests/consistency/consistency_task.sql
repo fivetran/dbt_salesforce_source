638af37312985c4c626d99385dd7729b5b48ec04
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-07-02 18:37:52 -0500

    add consistency tests

---
+++ integration_tests/tests/consistency/consistency_task.sql
@@ -0,0 +1,32 @@
+{{ config(
+    tags="fivetran_validations",
+    enabled=var('fivetran_validation_tests_enabled', false)
+) }}
+
+-- this test is to make sure the final columns are the same between versions
+with prod as (
+    select *
+    from {{ ref('stg_salesforce__task') }}
+),
+
+dev as (
+    select *
+    from {{ ref('stg_salesforce__task') }}
+),
+
+final as (
+    -- test will fail if any rows from prod are not found in dev
+    (select * from prod
+    except distinct
+    select * from dev)
+
+    union all -- union since we only care if rows are produced
+
+    -- test will fail if any rows from dev are not found in prod
+    (select * from dev
+    except distinct
+    select * from prod)
+)
+
+select *
+from final
