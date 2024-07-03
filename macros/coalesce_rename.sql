ad8202d57f401dd3fdcd77b6e23a39d6d1d95621
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-07-03 17:08:26 -0500

    update variable for readbility

--- macros/coalesce_rename.sql
+++ macros/coalesce_rename.sql
@@ -1,10 +1,10 @@
 {% macro coalesce_rename(
-    column_name,
+    column_key,
     column_dict,
-    original_column_name=column_dict[column_name]["name"],
-    datatype=column_dict[column_name]["datatype"],
-    alias=column_dict[column_name]["alias"] | default(original_column_name),
-    renamed_column_name=column_dict[column_name]["renamed_column_name"]
+    original_column_name=column_dict[column_key]["name"],
+    datatype=column_dict[column_key]["datatype"],
+    alias=column_dict[column_key]["alias"] | default(original_column_name),
+    renamed_column_name=column_dict[column_key]["renamed_column_name"]
     ) %}
 
 {# This macro accomodates Fivetran connectors that keep the original salesforce field naming conventions without underscores #}
@@ -16,5 +16,5 @@
         cast({{ original_column_name }} as {{ datatype }}))
         as {{ alias }}
 
-{% endif %}
+{%- endif %}
 {%- endmacro %}
