e0cf10fb81611023cfcab83e3e9dcbc5ad097aab
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-07-12 16:48:41 -0500

    update comments

--- macros/add_renamed_columns.sql
+++ macros/add_renamed_columns.sql
@@ -1,9 +1,10 @@
 {% macro add_renamed_columns(column_list) %}
+{# This macro determines the original names for each column and adds them to the list generated within each `get_*_columns` macro. By default, this macro processes column names by removing underscores and capitalizing each part that follows an underscore. This ensures all necessary columns are available for use in the `coalesce_rename` macro. Additionally, this macro tags each column with its renamed version to maintain tracking. #}
 
 {%- set renamed_columns = [] %}
 
 {%- for col in column_list %}
-    {# Use the alias if it is provided. #}
+
     {%- set original_column_name = col.name %}
 
     {%- if 'fivetran' not in original_column_name %}
