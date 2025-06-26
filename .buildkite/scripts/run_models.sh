26fd61180dcd7f81df9309378c18d04429f42fb4
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-06-25 22:53:12 -0500

    feature/freshness-updates

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -17,6 +17,7 @@ echo `pwd`
 cd integration_tests
 dbt deps
 dbt seed --target "$db" --full-refresh
+dbt source freshness --target "$db" || echo "...Only verifying freshness runs…"
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 dbt run --vars '{salesforce__user_role_enabled: false}' --target "$db" --full-refresh
